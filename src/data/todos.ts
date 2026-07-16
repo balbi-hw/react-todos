0e0764b779fefae9fd519d491588b76fcbae5a0b
hw <brightrain453@gmail.com>
2026-07-16 14:58:57 +0900

    data: add dummy todos data

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "../types/todos"
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "하이용",
+        completed: false
+    },
+    {
+        id: 2,
+        title: "좋아용",
+        completed: false
+    },
+    {
+        id: 3,
+        title: "잘가용",
+        completed: false
+    },
+]
